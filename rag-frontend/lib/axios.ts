8f0683682f9f345b251dcb44b86d38b021c0f71f
AmitVerma0903 <amitverma09736@gmail.com>
2026-06-05 17:42:15 +0530

    frontend url issue resolved

--- rag-frontend/lib/axios.ts
+++ rag-frontend/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const baseURL =
-  process.env.NEXT_PUBLIC_BACKEND_URL && process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_BACKEND_URL
+  process.env.NODE_ENV === "production"
+    ? "https://rag-production-9052.up.railway.app"
     : "http://localhost:8000"
 
 const axiosInstance = axios.create({
